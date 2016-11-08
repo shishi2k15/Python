dd091e0072e830bc764936e968a7086b2db83b7b
shishi2k15 <shivoskate@gmail.com>
2016-11-08 22:20:34 +0200

    Add desription

--- practice_python/character_input_01.py
+++ practice_python/character_input_01.py
@@ -1,3 +1,9 @@
+"""
+Create a program that asks the user to enter their name and their age.
+Print out a message addressed to them that tells them the year that they will
+turn 100 years old.
+"""
+
 from datetime import date
 
 current_year = int(date.today().year)
