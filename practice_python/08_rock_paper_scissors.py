ec836bb6304b7f2b6f8c27c208e0c91c2a04d9ec
shishi2k15 <shivoskate@gmail.com>
2016-11-12 14:55:39 +0200

    Add 8th exercise

--- practice_python/08_rock_paper_scissors.py
+++ practice_python/08_rock_paper_scissors.py
@@ -9,3 +9,25 @@ Rock beats scissors
 Scissors beats paper
 Paper beats rock
 """
+choice_dict = {'rock': 'scissors',
+               'scissors': 'paper',
+               'paper': 'rock'}
+
+
+def game(choice_1, choice_2):
+    if choice_1 == choice_2:
+        return 'Draw'
+    elif choice_dict[choice_1] == choice_2:
+        return 'Player 1 wins !'
+    else:
+        return 'Player 2 wins !'
+
+while True:
+    player_1 = str(raw_input('Player 1: '))
+    player_2 = str(raw_input('Player 2: '))
+
+    print(game(player_1, player_2))
+
+    play_again = str(raw_input('Play again ? y/n: \n'))
+    if play_again.lower() == 'n':
+        break
