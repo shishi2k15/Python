8c5010e473a56d1c5c1c1b784abc38d08a867150
shishi2k15 <shivoskate@gmail.com>
2016-11-14 01:26:01 +0200

    Add more functions

--- practice_python/11_check_primality_functions.py
+++ practice_python/11_check_primality_functions.py
@@ -3,7 +3,17 @@ Ask the user for a number and determine whether the number is prime or not.
 '''
 
 
+def get_int(user_input='Enter a number: '):
+    '''
+    Gets a string and turns it to integer number.
+    '''
+    return int(input(user_input))
+
+
 def is_prime(num):
+    '''
+    Returns True or False if the number is prime or not.
+    '''
     if num < 2:
         return False
     if num % 2 == 0:
@@ -15,6 +25,6 @@ def is_prime(num):
         div += 2
     return True
 
-user_input = int(input('Enter a number to check if its prime: '))
+user_input = get_int('Enter a number to check if its prime: ')
 
 print(is_prime(user_input))
