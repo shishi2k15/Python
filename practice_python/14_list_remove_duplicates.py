82fa25ac330e7eb0bf4d8935cb8310ed5e33676e
shishi2k15 <shivoskate@gmail.com>
2016-11-16 23:22:55 +0200

    14 added

--- practice_python/14_list_remove_duplicates.py
+++ practice_python/14_list_remove_duplicates.py
@@ -2,7 +2,17 @@
 Write a program (function!) that takes a list and returns a new list that
 contains all the elements of the first list minus all the duplicates.
 '''
+num_list = [1, 2, 3, 3, 3, 2, 5, 3, 4, 4]
+
+
+def dup_rem_loop(x):
+    y = []
+    y = [i for i in x if i not in y]
+    return y
 
 
 def dup_remove(a_list):
-    pass
+    return set(a_list)
+
+print(dup_remove(num_list))
+print(dup_remove(num_list))
